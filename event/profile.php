689539fbbd8f349a21614ededfa19f2b2980fdc2
gambitok <gambitokgd@gmail.com>
2020-05-19 13:08:37 +0300

    changed Profile, fixed Search

--- event/profile.php
+++ event/profile.php
@@ -1,15 +1,16 @@
 <?php
 
 if ($client->checkUnRegClient()) {
-//    $content=str_replace("{main_window}", $catalogue->getHtmlForm("profile/signin"), $content);
     header("Location: /signin", TRUE, 301);
 } else {
     $content=str_replace("{main_window}", $profile->showProfileForm(), $content);
     $content=str_replace("{profile_account}", $profile->showProfileAccount(), $content);
-    $content=str_replace("{profile_basket}", $shop->showMiniBasketForm()[0], $content);
+    $content=str_replace("{profile_basket}", $shop->showBasketForm(), $content);
     $content=str_replace("{profile_check}", $profile->showProfileCheck(), $content);
     $content=str_replace("{profile_orders}", $profile->showProfileOrders(), $content);
     $content=str_replace("{profile_file_list}", $profile->showPriceList(), $content);
+
+    //showMiniBasketForm()[0]
 
     $linksData=findLinks();
     $panel=$linksData[1]; if ($panel=="") $panel="account";
