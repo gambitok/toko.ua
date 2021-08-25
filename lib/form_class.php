3cd53ce315ae4d74ee06bbfac4e2c9e0096ade3c
gambitok <gambitokgd@gmail.com>
2021-08-25 17:00:09 +0300

    catalog applicable in article card

--- lib/form_class.php
+++ lib/form_class.php
@@ -230,6 +230,8 @@ class FormClass extends CatalogueClass
         $form = str_replace("{flag_visible}", "dnone", $form);
         $form = str_replace("{art_seo_text}", $this->getArticleSeoText($art_id, $h1), $form);
 
+        $form = str_replace("{applicable_form}", $this->getApplicableForm($art_id), $form);
+
         $form = $this->replaceLang($form);
 
         $breadcrumbs = $this->getArticleBreadCrumb($art_id, $article_nr_displ, $brand_id);
@@ -242,67 +244,134 @@ class FormClass extends CatalogueClass
         return compact("form", "title", "description", "breadcrumbs");
     }
 
+    public function getApplicableForm($art_id)
+    {
+        $typ_id = $this->getCookieAuto();
+        if ($typ_id != "") {
+            $automan = new AutoClass();
+            $typ_name = $automan->getCarDescription($typ_id);
+            if ($this->checkT2Link($typ_id, $art_id)) {
+                //success
+                $form = "<img src=\"/images/applicable/success.png\" alt=\"{applicable_success_cap}\"><span>{applicable_success_cap} $typ_name</span>";
+            } else {
+                $catalog = new CatalogueClass();
+                $group_id = $catalog->getArticleGroupExist($art_id);
+                $group_link = $this->getGroupRowLink($group_id);
+                //danger
+                $form = "
+                <div class=\"article-info-row__applicable-danger\">
+                    <div class=\"article-info-row__applicable-danger--row\">
+                        <img src=\"/images/applicable/danger.png\" alt=\"{applicable_danger_cap}\">
+                        <span>{applicable_danger_cap}</span>
+                    </div>
+                    <div class=\"article-info-row__applicable-danger--row\">
+                        <span>$typ_name <a onclick=\"deleteAutoGarage('$typ_id');\">&times;</a></span>
+                        
+                    </div>
+                    <div class=\"article-info-row__applicable-danger--row\">
+                        <a href=\"" . $this->getSiteLink() ."$this->catalog_link/$group_link/\" class=\"btn btn-info\" style='color:white'>{choose_product_car}</a>
+                        <button onclick=\"showGarageForm();\" class=\"btn btn-info btn-outline-info\">{choose_another_car}</button>
+                    </div>
+                </div>";
+            }
+        } else {
+            //warning
+            $form = "<img src=\"/images/applicable/warning.png\" alt=\"{applicable_warning_cap}\"><span>{applicable_warning_cap}</span>";
+        }
+        return $form;
+    }
+
+    public function getSeoLinkCatalog($group_id, $brand_id = 0)
+    {
+        $catalog = new CatalogueClass();
+        $group_name = $catalog->getGroupRowName($group_id);
+        $group_link = $catalog->getGroupRowLink($group_id);
+        $link = "<a href=\"" . $this->getSiteLink() . "$this->catalog_link/$group_link/\">$group_name</a>";
+        if ($brand_id > 0) {
+            $brand_name = $this->getBrandName($brand_id);
+            $brand_link = $this->getBrandLink($brand_id);
+            $link = "<a href=\"" . $this->getSiteLink() . "$this->catalog_link/$group_link/brandy=$brand_link/\">$brand_name</a>";
+        }
+        return $link;
+    }
+
+    public function getSeoLinkArticle($art_id_sel, $brand_id_sel)
+    {
+        $article_search = $this->getArticleSearch($art_id_sel);
+        $article_displ = $this->getArticleDispl($art_id_sel);
+        $article_name = $this->getArticleName($art_id_sel);
+        $brand_name = $this->getBrandName($brand_id_sel);
+        $brand_search = $this->getUrlString($brand_name);
+        return "<a href=\"" . $this->getSiteLink() . "$this->article_link/$article_search/$brand_search/$art_id_sel/\">$article_name $brand_name $article_displ</a>";
+    }
+
     public function getArticleSeoText($art_id, $h1)
     {
         $db = DbSingleton::getTokoDb();
         $dbc = DbSingleton::getTokoCacheDb();
         $catalog = new CatalogueClass();
-        $catalog_exist = new CatalogExistClass();
         $r = $db->query("SELECT `TEXT` FROM `SEO_STR_ARTICLES` WHERE `ART_ID` = $art_id LIMIT 1;");
         $n = $db->num_rows($r);
         if ($n > 0) {
             $form = $db->result($r, 0, "TEXT");
         } else {
-            $form = "{_still_search} {Main_Category_H1}? {_go_store_toko} {_toko} {_choose_best} {GET_PAGE_H1}. 
+            $form = "
+            {_still_search} {Main_Category_H1}? {_go_store_toko} {_toko} {_choose_best} {GET_PAGE_H1}. 
             {_lowest_prices} {Product_1} {_high_quality_category} {Product_Category_H1}. 
             {_cooperate} {_popular_brands} {Tags_brand_1} {and_cap} {Tags_brand_2}, {_presented_in_section} {Cat_random1} {and_cap} {Cat_random2}. 
-            {_right_choice} {GET_PAGE_H2}. {_fast_order} {Geo_nominative} {_other_cities}";
+            {_right_choice} {GET_PAGE_H2}. 
+            {_fast_order} {Geo_nominative} {_other_cities}";
 
             $form = str_replace("{GET_PAGE_H1}", $h1, $form);
 
             $group_id = $catalog->getArticleGroupExist($art_id);
-            $group_name = $catalog->getGroupRowName($group_id);
-            $parrent_h1 = $group_name;
-            $form = str_replace("{Main_Category_H1}", $parrent_h1, $form);
+            $form = str_replace("{Main_Category_H1}", $this->getSeoLinkCatalog($group_id), $form);
 
             $r = $dbc->query("SELECT `art_id`, `brand_id`, `group_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `group_id` != $group_id ORDER BY RAND() LIMIT 1;");
             $art_id_sel = $db->result($r, 0, "art_id");
             $brand_id_sel = $db->result($r, 0, "brand_id");
             $group_id_sel = $db->result($r, 0, "group_id");
-            $form = str_replace("{Product_1}", $this->getBrandName($brand_id_sel) . " " . $this->getArticleDispl($art_id_sel), $form);
-            $form = str_replace("{Product_Category_H1}", $this->getGroupRowName($group_id_sel), $form);
+            $form = str_replace("{Product_1}", $this->getSeoLinkArticle($art_id_sel, $brand_id_sel), $form);
 
-            $r = $dbc->query("SELECT `brand_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `group_id` != $group_id AND `group_id` != $group_id_sel ORDER BY RAND() LIMIT 2;");
+            $group_id_sel_name = $this->getGroupRowName($group_id_sel);
+            $group_id_sel_link = $this->getGroupRowLink($group_id_sel);
+            $parrent_group = "<a href=\"" . $this->getSiteLink() . "$this->catalog_link/$group_id_sel_link/\">$group_id_sel_name</a>";
+            $form = str_replace("{Product_Category_H1}", $parrent_group, $form);
+
+            $r = $dbc->query("SELECT `brand_id`, `group_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `group_id` != $group_id AND `group_id` != $group_id_sel ORDER BY RAND() LIMIT 2;");
             $n = $dbc->num_rows($r);
-            $brand_id_arr = [];
+            $arr = [];
             for ($i = 1; $i <= $n; $i++) {
-                $brand_id_arr[] = $dbc->result($r, $i - 1, "brand_id");
+                $arr[] = ["group_id" => $dbc->result($r, $i - 1, "group_id"), "brand_id" => $dbc->result($r, $i - 1, "brand_id")];
             }
-            $brand_id_sel1 = $brand_id_arr[0];
-            $brand_id_sel2 = $brand_id_arr[1];
-            $form = str_replace("{Tags_brand_1}", $this->getBrandName($brand_id_sel1), $form);
-            $form = str_replace("{Tags_brand_2}", $this->getBrandName($brand_id_sel2), $form);
+            $form = str_replace("{Tags_brand_1}", $this->getSeoLinkCatalog($arr[0]["group_id"], $arr[0]["brand_id"]), $form);
+            $form = str_replace("{Tags_brand_2}", $this->getSeoLinkCatalog($arr[1]["group_id"], $arr[1]["brand_id"]), $form);
 
+            $brand_id_sel1 = $arr[0]["brand_id"];
             $r = $dbc->query("SELECT `group_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `brand_id` = $brand_id_sel1 ORDER BY RAND() LIMIT 1;");
             $group_id_sel = $dbc->result($r, 0, "group_id");
-            $form = str_replace("{Cat_random1}", $this->getGroupRowName($group_id_sel), $form);
-            $form = str_replace("{GET_PAGE_H2}", $catalog_exist->getCatalogH1($group_id_sel), $form);
+            $form = str_replace("{Cat_random1}", $this->getSeoLinkCatalog($group_id_sel), $form);
 
+            $r = $dbc->query("SELECT `art_id`, `brand_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `group_id` = $group_id_sel ORDER BY RAND() LIMIT 1;");
+            $art_id_sel = $db->result($r, 0, "art_id");
+            $brand_id_sel = $db->result($r, 0, "brand_id");
+            $form = str_replace("{GET_PAGE_H2}", $this->getSeoLinkArticle($art_id_sel, $brand_id_sel), $form);
+
+            $brand_id_sel2 = $arr[1]["brand_id"];
             $r = $dbc->query("SELECT `group_id` FROM `EX_TABLE_TREE_AVAILABLE` WHERE `brand_id` = $brand_id_sel2 ORDER BY RAND() LIMIT 1;");
-            $group_id_sel = $dbc->result($r, 0, "group_id");
-            $form = str_replace("{Cat_random2}", $this->getGroupRowName($group_id_sel), $form);
+            $group_id_sel2 = $dbc->result($r, 0, "group_id");
+            $form = str_replace("{Cat_random2}", $this->getSeoLinkCatalog($group_id_sel2), $form);
 
             $r = $db->query("SELECT `CITY_NAME` FROM `SEO_LISTING_CITY` ORDER BY RAND() LIMIT 1;");
             $random_city = $db->result($r, 0, "CITY_NAME");
             $form = str_replace("{Geo_nominative}", $random_city, $form);
 
-            $r = $db->query("SELECT `LIST_KEY` FROM `SEO_LISTING`;");
+            $r = $db->query("SELECT `LIST_KEY` FROM `SEO_LISTING` WHERE 1;");
             $langVariables = array_column(mysqli_fetch_all($r), 0);
 
             foreach ($langVariables as $langVariable) {
                 $form = str_replace("{" . $langVariable . "}", $this->getSeoListingName($langVariable), $form);
             }
-
             $form = $this->replaceLang($form);
 
             //$db->query("INSERT INTO `SEO_STR_ARTICLES` (`ART_ID`, `TEXT`) VALUES ($art_id, '$form');");
